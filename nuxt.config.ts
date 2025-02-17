aaa582c9fb4212a8a2dde14c37e7c00c613324bf
Fllugel <135070466+Fllugel@users.noreply.github.com>
2025-02-18 01:37:32 +0200

    deploy command added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,10 @@ export default defineNuxtConfig({
     componentDir: './components/ui',
   },
 
+  app: {
+    baseURL: '/AI-SHOP-ASSISTANT-UI/',
+    buildAssetsDir: 'assets',
+  },
+
   compatibilityDate: '2025-02-15',
 })
