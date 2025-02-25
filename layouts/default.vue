7b42b06a56c43be421454f7a28ec4a01b54c57dd
Fllugel <135070466+Fllugel@users.noreply.github.com>
2025-02-25 14:55:39 +0200

    add open sidebar button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex bg-black text-white">
     <SidebarProvider>
-      <Sidebar class="flex border-white flex-col">
+      <Sidebar collapsible="offcanvas" variant="sidebar" class="flex border-white flex-col">
         <div>
-          <SidebarHeader class="flex items-center flex-row justify-center border-b">
+          <SidebarHeader class="flex items-center flex-row justify-center border-b text-white">
             <Icon name="heroicons-solid:academic-cap" class="mr-2 h-5 w-5" />
             <h1 class="text-2xl font-bold">Store ChatBot</h1>
           </SidebarHeader>
@@ -31,10 +31,12 @@
       </Sidebar>
 
       <SidebarInset>
+        <SidebarTrigger class="absolute top-5 left-5 z-50 w-10 h-10" />
         <keep-alive>
           <slot />
         </keep-alive>
       </SidebarInset>
+
     </SidebarProvider>
   </div>
 </template>
